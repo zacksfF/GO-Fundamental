38cf71302a3fa238930f71cc9928df4ac05629c4
zakaria saif <129240583+zacksfF@users.noreply.github.com>
2023-09-23 14:11:32 +0100

    Add files via upload

---
+++ variable.go/part 3/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 30
+	fmt.Println("may age is : ", age)
+
+	var school bool = false
+	fmt.Println(school)
+
+	Dave := true
+	fmt.Println("Dave love pizza true or false :", Dave)
+}
---
+++ variable.go/part 3/multiple.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	A := "Go programmer"
+	fmt.Println(A)
+
+	car, cost := "BMW", 100000
+	fmt.Println(car, cost)
+	car, year := "Audi", 2018
+	_ = year
+	
+}
